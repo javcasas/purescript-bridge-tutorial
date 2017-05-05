e79b17e45403fbcbe1bb98d32ecd20749fff483d
Javier Casas <javcasas@gmail.com>
2017-05-05 17:21:33 -0500

    Multi-name scientists

--- backend/src/Lib.hs
+++ backend/src/Lib.hs
@@ -14,8 +14,7 @@ import Network.Wai.Middleware.Cors
 
 data Scientist = Scientist
   { sId        :: Int
-  , sFirstName :: String
-  , sLastName  :: String
+  , sNames     :: [String]
   } deriving (Eq, Show)
 
 $(deriveJSON defaultOptions ''Scientist)
@@ -38,8 +37,10 @@ server :: Server API
 server = return scientists
 
 scientists :: [Scientist]
-scientists = [ Scientist 1 "Isaac" "Newton"
-             , Scientist 2 "Albert" "Einstein"
-             , Scientist 3 "Gottfried" "Leibniz"
-             , Scientist 4 "Stephen" "Hawking"
+scientists = [ Scientist 1 ["Isaac", "Newton"]
+             , Scientist 2 ["Albert", "Einstein"]
+             , Scientist 3 ["Gottfried", "Wilhelm", "Leibniz"]
+             , Scientist 4 ["Stephen", "Hawking"]
+             , Scientist 5 ["Pythagoras"]
+             , Scientist 6 ["Wernher", "von", "Braun"]
              ]
