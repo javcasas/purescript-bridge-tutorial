6c60618e93271fafc960aca8e2ef0318b73f8713
Javier Casas <javcasas@gmail.com>
2017-06-23 08:39:56 -0500

    Drop ID, add photo

--- backend/src/Types.hs
+++ backend/src/Types.hs
@@ -7,17 +7,18 @@ import Data.Aeson.TH
 import GHC.Generics (Generic)
 
 data Scientist = Scientist
-  { sId        :: Int
-  , sNames     :: [String]
+  {
+    sNames     :: [String]
+  , sPhotoUrl  :: String
   } deriving (Eq, Show, Generic)
 
 $(deriveJSON defaultOptions ''Scientist)
 
 scientists :: [Scientist]
-scientists = [ Scientist 1 ["Isaac", "Newton"]
-             , Scientist 2 ["Albert", "Einstein"]
-             , Scientist 3 ["Gottfried", "Wilhelm", "Leibniz"]
-             , Scientist 4 ["Stephen", "Hawking"]
-             , Scientist 5 ["Pythagoras"]
-             , Scientist 6 ["Wernher", "von", "Braun"]
+scientists = [ Scientist ["Isaac", "Newton"] "https://upload.wikimedia.org/wikipedia/commons/3/39/GodfreyKneller-IsaacNewton-1689.jpg"
+             , Scientist ["Albert", "Einstein"] "https://upload.wikimedia.org/wikipedia/commons/d/d3/Albert_Einstein_Head.jpg"
+             , Scientist ["Gottfried", "Wilhelm", "Leibniz"] "http://apusepress.booktype.pro/early-readings-in-the-philosophy-of-science/gottfried-wilhelm-leibniz-1646-1716/static/Leibniz.jpg"
+             , Scientist ["Stephen", "Hawking"] "https://upload.wikimedia.org/wikipedia/commons/e/eb/Stephen_Hawking.StarChild.jpg"
+             , Scientist ["Pythagoras"] "http://www.thefamouspeople.com/profiles/images/pythagoras-4.jpg"
+             , Scientist ["Wernher", "von", "Braun"] "https://upload.wikimedia.org/wikipedia/commons/5/56/Wernher_von_Braun_1960.jpg"
              ]
